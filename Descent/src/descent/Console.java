b340fcb0e88fba5f990b7543dd81f86c6bd66eeb
2301932-boop <2301932@school.ecsd.net>
2026-04-17 09:12:00 -0600

    Update Console.java

--- Descent/src/descent/Console.java
+++ Descent/src/descent/Console.java
@@ -3,28 +3,30 @@ package descent;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-
 public class Console {
-
-	// String colours/formats ==============================================================
+	// String colours/formats
+	// ==============================================================
 	public static final String RESET = "\u001B[0m";
-	public static final String RED = "\u001B[31m";
-	public static final String GREEN = "\u001B[32m";
-	public static final String YELLOW = "\u001B[33m";
-	public static final String BLUE = "\u001B[34m";
-	public static final String PURPLE = "\u001B[35m";
-	public static final String CYAN = "\u001B[36m";
-	public static final String WHITE = "\u001B[37m";
+	public static final String RED = "\u001B[91m";
+	public static final String GREEN = "\u001B[92m";
+	public static final String YELLOW = "\u001B[93m";
+	public static final String BLUE = "\u001B[94m";
+	public static final String PURPLE = "\u001B[95m";
+	public static final String CYAN = "\u001B[96m";
+	public static final String GREY = "\u001B[90m";
+	public static final String WHITE = "\u001B[97m";
+	public static final String BOLD_RED = "\u001B[1;91m";
+	public static final String BOLD_GREEN = "\u001B[1;92m";
+	public static final String BOLD_YELLOW = "\u001B[1;93m";
+	public static final String BOLD_BLUE = "\u001B[1;94m";
+	public static final String BOLD_PURPLE = "\u001B[1;95m";
+	public static final String BOLD_CYAN = "\u001B[1;96m";
+	public static final String BOLD_GREY = "\u001B[1;90m";
+	public static final String BOLD_WHITE = "\u001B[1;97m";
 
-	public static final String BOLD_RED = "\u001B[1;31m";
-	public static final String BOLD_GREEN = "\u001B[1;32m";
-	public static final String BOLD_YELLOW = "\u001B[1;33m";
-	public static final String BOLD_WHITE = "\u001B[1;37m";
-	
 	public static final String ITALIC = "\u001B[3m";
-	//===========================================================================================
-	
-	
+	// ===========================================================================================
+
 	private static final Scanner in = new Scanner(System.in);
 
 	// basic error checking Int
@@ -33,8 +35,7 @@ public class Console {
 			System.out.print(prompt);
 			if (in.hasNextInt()) {
 				int num = in.nextInt();
-
-				in.nextLine(); 
+				in.nextLine();
 				if (num >= min && num <= max) {
 					return num;
 				}
@@ -46,11 +47,11 @@ public class Console {
 		}
 	}
 
-	// For open ended strings where anything can go
+	// For open ended strings where anything can go (ex. names)
 	public static String errCheckString(String prompt) {
 		while (true) {
 			System.out.print(prompt);
-			String input = in.nextLine().trim(); 
+			String input = in.nextLine().trim();
 			if (!input.isEmpty()) {
 				return input;
 			}
@@ -72,52 +73,78 @@ public class Console {
 			System.out.println("Choose from: " + String.join(", ", validOptions));
 		}
 	}
-	
+
 	public static void pressEnter() {
-	    System.out.println(Console.WHITE + "\n[ Press Enter to continue... ]" + Console.RESET);
-	    in.nextLine(); // Waits for the user to hit Enter
+		System.out.println(Console.WHITE + "\n[ Press Enter to continue... ]" + Console.RESET);
+		in.nextLine(); 
 	}
 
 	public static void clear() {
-	    // This works in most modern terminals (VS Code, IntelliJ, Putty)
-	    System.out.print("\033[H\033[2J");
-	    System.out.flush();
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
 	}
-	
+
 	// Coloured strings method
 	public static void println(String colour, String message) {
 		System.out.println(colour + message + RESET);
 	}
-	
+
 	public static void printSlow(String msg, int time) {
-	    printSlowClr(WHITE, msg, time); 
-	}
-	
-	public static void printSlow(String msg) {
-	    printSlowClr(WHITE, msg, 25); 
-	}
-	
-	public static void printSlowClr(String colour, String msg, int time) {
-		try {
-	        // 1. Set the color once before the typing starts
-	        System.out.print(colour);
-	        
-	        char[] array = msg.toCharArray();
-	        for (int i = 0; i < array.length; i++) {
-	            System.out.print(array[i]);
-	            // Ensure each character appears the moment it's printed
-	            System.out.flush(); 
-	            TimeUnit.MILLISECONDS.sleep(time);    
-	        }
-	        
-	        // 2. Reset the color and move to a new line at the end
-	        System.out.println(RESET);
-	        
-	    } catch (Exception e) {
-	        // Fallback if the sleep is interrupted
-	        System.out.println(colour + msg + RESET);
-	    }
-	
+		printSlowClr(WHITE, msg, time);
 	}
 
+	public static void printSlow(String msg) {
+		printSlowClr(WHITE, msg, 25);
+	}
+
+	public static void printSlowClr(String colour, String msg, int time) {
+		try {
+			//Set the color once before the typing starts
+			System.out.print(colour);
+
+			char[] array = msg.toCharArray();
+			for (int i = 0; i < array.length; i++) {
+				System.out.print(array[i]);
+				TimeUnit.MILLISECONDS.sleep(time);
+			}
+
+			System.out.println(RESET);
+
+		} catch (Exception e) {
+			System.out.println(colour + msg + RESET);
+		}
+
+	}
+	
+	public static void showColours() {
+		println(RED, "This is a show of colours!");
+		println(GREEN, "This is a show of colours!");
+		println(YELLOW, "This is a show of colours!");
+		println(BLUE, "This is a show of colours!");
+		println(PURPLE, "This is a show of colours!");
+		println(CYAN, "This is a show of colours!");
+		println(GREY, "This is a show of colours!");
+		println(WHITE, "This is a show of colours!");
+		
+		println(BOLD_RED, "This is a show of colours!");
+		println(BOLD_GREEN, "This is a show of colours!");
+		println(BOLD_YELLOW, "This is a show of colours!");
+		println(BOLD_BLUE, "This is a show of colours!");
+		println(BOLD_PURPLE, "This is a show of colours!");
+		println(BOLD_CYAN, "This is a show of colours!");
+		println(BOLD_GREY, "This is a show of colours!");
+		println(BOLD_WHITE, "This is a show of colours!");
+
+		
+	}
+	
+	//stop time in milliseconds
+	public static void sleep(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
